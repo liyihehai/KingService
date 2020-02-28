25f5b50ae3d3a894d0389fc0c15eadb2600520e1
liyihehai <liyi_hehai@sina.com>
2020-02-28 21:07:32 +0800

    外部tomcat运行

--- src/main/java/com/nnte/kservice/KingServiceApplication.java
+++ src/main/java/com/nnte/kservice/KingServiceApplication.java
@@ -1,16 +1,25 @@
 package com.nnte.kservice;
 
+import com.nnte.OfficeConverPDF.OfficeConverPDFConfig;
+import com.nnte.framework.utils.NumberUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("com.nnte")
 public class KingServiceApplication extends SpringBootServletInitializer
+        implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
 {
+    @Autowired
+    public KingServiceConfig kingServiceConfig;
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
         return application.sources(KingServiceApplication.class);
@@ -19,5 +28,11 @@ public class KingServiceApplication extends SpringBootServletInitializer
     public static void main(String[] args)
     {
         SpringApplication.run(KingServiceApplication.class, args);
+    }
+
+    //下边customize方法实现EmbeddedServletContainerCustomizer 接口,实现更改tomcat端口号的需求
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        factory.setPort(NumberUtil.getDefaultInteger(kingServiceConfig.getPort()));
     }
 }
--- src/main/java/com/nnte/kservice/KingServiceConfig.java
+++ src/main/java/com/nnte/kservice/KingServiceConfig.java
@@ -1,12 +1,29 @@
 package com.nnte.kservice;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
+@ConfigurationProperties(prefix = "server")
+@PropertySource(value = "classpath:application.properties")
+
 public class KingServiceConfig {
     @Bean(initMethod = "initMain")
     public KingServiceCompnent kingServiceCompnent(){
         return new KingServiceCompnent();
+    }
+
+    private String port;
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
     }
 }
